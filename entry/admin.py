411c1ae11010eee0813795e5ec6b00930d12d913
Anish <anishjoshi510@gmail.com>
2023-08-03 16:04:08 +0545

    admin modification

--- entry/admin.py
+++ entry/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.User),
-admin.site.register(models.Attendance),
-admin.site.register(models.Timesheet),
-admin.site.register(models.TimesheetEntries),
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('first_name','last_name','email_address')
+    
+@admin.register(models.Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ('user','date','punch_in','punch_out','break_in','break_out')
+    
+@admin.register(models.Timesheet)
+class TimesheetAdmin(admin.ModelAdmin):
+    list_display = ('user','date')
+    
+@admin.register(models.TimesheetEntries)
+class TimesheetEntriesAdmin(admin.ModelAdmin):
+    list_display = ('entry_type','entry_time')
